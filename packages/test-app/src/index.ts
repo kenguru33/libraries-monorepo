73a861c16bace958f59a15f8f0a93f264aad344f
Bernt Anker <bernt.anker@me.com>
2020-09-21 20:13:47 +0200

    test app modifed

--- packages/test-app/src/index.ts
+++ packages/test-app/src/index.ts
@@ -1,19 +1,24 @@
 import { IsAfterDate } from 'class-validator-date';
 import { IsString, validateSync } from 'class-validator';
+import {IsCog, IsMmsi} from 'class-validator-ais'
 
 export class Meeting {
   @IsString()
-  name: string;
+  name: number;
   startTime: string;
-
+  @IsMmsi()
+  mmsi: string
+  @IsCog()
+  hastighet: number
   @IsAfterDate('endTime')
   endTine: string;
 }
 
 const meeting = new Meeting();
-meeting.name = 'Møte';
+meeting.name = 12;
+meeting.mmsi = '123d'
 meeting.startTime = '2020-04-30T09:45:00';
-meeting.endTine = '2020-04-30T09:45:00';
+meeting.endTine = '2020-04-30T09:46:00';
 
 const error = validateSync(meeting);
 console.log(error);
